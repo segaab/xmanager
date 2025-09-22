b65beb9c784893a0991ab31a2fafe3b25e7cc5f0
segaab <segaab120@gmail.com>
2025-09-21 19:29:19 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,20 +7,6 @@ def purged_train_test_split(events_df: pd.DataFrame, purge_radius: int = 10):
     Performs a naive purged train-test split.
     The first 80% of events are assigned to training, the remaining 20% to testing.
     The purge_radius parameter is included for API compatibility with more sophisticated purging schemes.
-    
-    Parameters:
-    -----------
-    events_df : pd.DataFrame
-        DataFrame containing event data. Index is assumed to be sorted chronologically.
-    purge_radius : int
-        Number of events to purge around the test set. Currently not applied (placeholder).
-    
-    Returns:
-    --------
-    train_mask : np.ndarray
-        Boolean array where True indicates a training event.
-    test_mask : np.ndarray
-        Boolean array where True indicates a testing event.
     """
     n = len(events_df)
     if n == 0:
